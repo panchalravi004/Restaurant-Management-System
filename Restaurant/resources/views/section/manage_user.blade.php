730da49cc545577e902a4a9301e837e5f289f7e6
panchalravi004 <panchalravi018@gmail.com>
2022-11-18 10:48:29 +0530

    change method of user update

--- Restaurant/resources/views/section/manage_user.blade.php
+++ Restaurant/resources/views/section/manage_user.blade.php
@@ -78,7 +78,6 @@
         <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
             <form class="modal-content" method="post" action="{{ route('edit_user',['id'=>$u->id])}}">
                 @csrf
-                @method('PATCH')
                 <div class="modal-header">
                     <h5 class="modal-title">Edit User | {{ucwords($u->name)}}</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
